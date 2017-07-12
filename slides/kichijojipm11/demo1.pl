96eef6d164f279ac04eabe196d62b4122e007ac4
Kobayasi, Hiroaki <buribullet@gmail.com>
2017-07-12 19:23:55 +0900

    デモコードを元に戻す

--- slides/kichijojipm11/demo1.pl
+++ slides/kichijojipm11/demo1.pl
@@ -2,17 +2,17 @@ use strict;
 
 package CD {
   # ↓↓ここでクラス CD の要素を宣言↓↓
-  use fields qw/tile artist year/;
+  use fields qw/title artist year/;
 }
 
 # ↓↓ my変数宣言にクラス CD を結びつけると…
 my CD $cd = +{};
 
 # ↓↓HASH要素の参照に typo 検査が、効く
-$cd->{tile} = "bar";   # Ok
+$cd->{title} = "bar";   # Ok
 
 # ↓↓このように typo するとコンパイルエラー
-$cd->{tile} = "bar"; # COMPILE ERROR!
+# $cd->{titleee} = "bar"; # COMPILE ERROR!
 
 
 
