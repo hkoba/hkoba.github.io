814841e9176344e5b1f01bac750516b439a9db8b
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-11-30 21:23:00 +0900

    Add built book

---
+++ perl/adv2021-1/book/eg-hello/hello.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+def run()
+  puts 'Hello world!'
+end
+
+run() if __FILE__ == $0
---
+++ perl/adv2021-1/book/eg-hello/hello.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ts-node
+
+export function run() {
+  console.log('Hello world!')
+}
+
+if (module.id === ".") {
+  run()
+}
